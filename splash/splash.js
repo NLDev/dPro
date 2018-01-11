5f9bcb1fcf3af0e43e42db33e0cf1d66d8752c44
NullDev (Shadow) <NL-Dev@yandex.com>
2018-01-11 23:05:45 +0100

    code for splash screen skip

--- splash/splash.js
+++ splash/splash.js
@@ -6,13 +6,16 @@
 //-----------------------//
 ///////////////////////////
 
+const remote = require("electron").remote;
 window.$ = window.jQuery = require("jquery");
+
+let arg = remote.getGlobal("sharedObject").prop;
 
 let next = function(){ window.location = "../main/main.html"; };
 
 window.onload = () => {
     $(document).ready(function() {
-        setTimeout(next, 5000);
+        (!!~arg.indexOf("--skip-splash") || !!~arg.indexOf("-s")) ? next() : setTimeout(next, 5000);
         return;
     });
 }
