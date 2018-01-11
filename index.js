bf1e5daa6411a7027aef1606f227e4df68e6ac7b
NullDev (Shadow) <NL-Dev@yandex.com>
2018-01-11 14:36:48 +0100

    Added base code for initial script

---
+++ index.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const { app, BrowserWindow } = require("electron");
+const path                   = require("path");
+const { shell }              = require("electron");
+const os                     = require("os");
+const ipc                    = require("electron").ipcMain;
+
+const debug = true;
+
+if (debug){
+    require("electron-context-menu")({
+        prepend: (params, browserWindow) => [{
+            label: "Rainbow",
+            visible: params.mediaType === "image"
+        }]
+    });
+}
+
+function getLogoPath(){
+    if (os.platform() === "darwin")     return "/icon/icon.icns";
+    else if (os.platform() === "win32") return "/icon/icon.ico";
+    else                                return "/icon/icon.png";
+}
+
+function createWindowConfig(){
+    let conf = { 
+        resizable: true,
+        minHeight: 600,
+        minWidth:  1000, 
+        width:     1000, 
+        heigth:    600, 
+        icon:      __dirname + getLogoPath(), 
+        show:      false 
+    };
+    return conf;
+}
+
+ipc.on("UI-windowID", function (event) { event.returnValue = win.id; });
+
+app.setName("dPro");
+
+app.on("ready", () => {
+    console.log(
+        "                         \n" +
+        "    ╔═╗╔╦╗╔═╗╦═╗╔╦╗╔═╗╔╦╗\n" +
+        "    ╚═╗ ║ ╠═╣╠╦╝ ║ ║╣  ║║\n" +
+        "    ╚═╝ ╩ ╩ ╩╩╚═ ╩ ╚═╝═╩╝\n"
+    );
+
+    let win = new BrowserWindow(createWindowConfig());
+    win.setMenu(null); 
+    win.loadURL(`file://${__dirname}/main.html`);
+    win.on("ready-to-show", () => { win.show(); });
+    win.on("closed",        () => { app.quit(); });
+    win.center();
+});
