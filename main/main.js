80ca95f7fc52090eac8f502fd6540da9923c5ee6
NullDev (Shadow) <NL-Dev@yandex.com>
2018-01-12 15:13:01 +0100

    Added main data API

--- main/main.js
+++ main/main.js
@@ -12,9 +12,72 @@ const { app, BrowserWindow }  = require("electron");
 
 window.$ = window.jQuery = require("jquery");
 
-window.onload = () => {
-    let win = remote.getCurrentWindow();
-	let currentWindow = remote.getCurrentWindow().removeAllListeners();
+var MONTHS = ["{{ZEIT}}"];
+var config = {
+    type: 'line',
+    data: {
+        labels: ["15", "15", "15", "15", "15", "15", "15"],
+        datasets: [{
+            label: "",
+            backgroundColor: window.chartColors.red,
+            borderColor: window.chartColors.red,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ],
+            fill: false,
+        }]
+    },
+    options: {
+            responsive: true,
+    title:{
+            display:true,
+            text: "{{TITLE}}"
+        },
+        tooltips: {
+            mode: "index",
+            intersect: false,
+        },
+        hover: {
+            mode: "nearest",
+            intersect: true
+        },
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: "{{ZEIT}}"
+                }
+            }],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: "{{MENGE}}"
+                }
+            }]
+        }
+    }
+};
 
-win.setTitle(win.getTitle() + " - Main");
-}
+window.onload = function() {
+    var ctx = document.getElementById("canvas").getContext("2d");
+    window.myLine = new Chart(ctx, config);
+};
+
+document.getElementById('randomizeData').addEventListener('click', function() {
+    config.data.datasets.forEach(function(dataset) {
+        dataset.data = dataset.data.map(function() {
+            return randomScalingFactor();
+        });
+    });
+    window.myLine.update();
+});
+
+var colorNames = Object.keys(window.chartColors);
