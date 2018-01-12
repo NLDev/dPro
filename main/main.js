3e8fba3bb05156b5a5b61447c5193723b6180808
NullDev (Shadow) <NL-Dev@yandex.com>
2018-01-12 22:36:10 +0100

    Added modals

--- main/main.js
+++ main/main.js
@@ -9,6 +9,7 @@
 const { ipcRenderer } = require("electron");
 const remote = require("electron").remote;
 const { app, BrowserWindow }  = require("electron");
+const shell = require("electron").shell;
 
 window.$ = window.jQuery = require("jquery");
 
@@ -66,26 +67,49 @@ var config = {
     }
 };
 
-$(document).ready(function(){
-	function classCheck(){
-        if ($("#menu-drawer").hasClass("open")){
-            $("#menu-drawer").removeClass("open");
-            $("#menu-drawer").addClass("close");
-        }
-        else {
-            $("#menu-drawer").removeClass("close");
-            $("#menu-drawer").addClass("open")
-        }
-        if ($("#overlay").hasClass("over")){
-            $("#overlay").removeClass("over");
-            $("#overlay").addClass("under");
-        }
-        else {
-            $("#overlay").removeClass("under");
-            $("#overlay").addClass("over")
-        }
-    }
+function msg(msg){
+    classCheck();
+    $(".container").removeClass("change");
+    $("#nlmsg-text").html(msg);
+    $(".nlmsg").addClass("nlmsg-show");
+    setTimeout(function(){ $(".nlmsg").removeClass("nlmsg-show"); }, 3000);
+}
 
+function modal(msg, headeroff, footeroff){
+    classCheck();
+    $(".container").removeClass("change");
+    if (headeroff) $(".nlmodal-header").css("display", "none");
+    if (footeroff) $(".nlmodal-footer").css("display", "none");
+    $(".nlmodal").css("display", "block");
+    $("#nlmodal-text").html(msg);
+}
+
+function forceModalClose(){ 
+    $(".nlmodal").css("display", "none"); 
+    $(".nlmodal-header").css("display", "block");
+    $(".nlmodal-footer").css("display", "block");
+}
+
+function classCheck(){
+    if ($("#menu-drawer").hasClass("open")){
+        $("#menu-drawer").removeClass("open");
+        $("#menu-drawer").addClass("close");
+    }
+    else {
+        $("#menu-drawer").removeClass("close");
+        $("#menu-drawer").addClass("open")
+    }
+    if ($("#overlay").hasClass("over")){
+        $("#overlay").removeClass("over");
+        $("#overlay").addClass("under");
+    }
+    else {
+        $("#overlay").removeClass("under");
+        $("#overlay").addClass("over")
+    }
+}
+
+$(document).ready(function(){
     $(".container").on("click", function(){
         $(this).toggleClass("change");
         classCheck();
@@ -95,6 +119,34 @@ $(document).ready(function(){
         $(".container").toggleClass("change");
         classCheck();
     });
+
+    $("#code").on("click", function(){
+        shell.openExternal("https://github.com/NLDev/dPro");
+        modal(
+            "Opening link in browser..." +
+            "<br><br>" +
+            "Note: This may take a while depending on your standard browser."
+        );
+    });
+
+    $("#about").on("click", function(){
+        modal(
+            "some info lmao",
+            false,
+            true
+        );
+    });
+
+    $("#exit").on("click", function(){
+        classCheck();
+        $(".container").removeClass("change");
+        msg("Exiting...");
+        let winObj = remote.getCurrentWindow();
+        setTimeout(function(){ winObj.close(); }, 1500);
+    });
+
+    $(".nlmodal-close").click(function(e) { forceModalClose(); });
+    window.onclick = function(event) { if (event.target == document.getElementById("nlmodal")) forceModalClose(); }
 });
 
 window.onload = function() {
