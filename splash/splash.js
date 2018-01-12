0527def788c79eb28baf21bd64b821ba244eacea
NullDev (Shadow) <NL-Dev@yandex.com>
2018-01-12 15:13:11 +0100

    Test

--- splash/splash.js
+++ splash/splash.js
@@ -11,8 +11,10 @@ const remote = require("electron").remote;
 window.$ = window.jQuery = require("jquery");
 
 let lowrs = remote.getGlobal("sharedObject").lowres;
+let win = remote.getCurrentWindow();
 
 window.onload = () => {
+    win.center();
     $(document).ready(function() {
         if (lowrs) $("body").addClass("low-res");
     });
